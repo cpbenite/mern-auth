cf2de1bd7a505183fc23309416566bf28cfa84cc
christian.benitez <christian.p.benitez@gmail.com>
2023-06-19 19:17:20 -0700

    Prepare for production.

--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,11 @@
-import express from 'express';
-import dotenv from 'dotenv';
-dotenv.config();
+import path from 'path'
+import express from 'express'
+import dotenv from 'dotenv'
+dotenv.config()
 import cookieParser from 'cookie-parser'
 import userRoutes from './routes/userRoutes.js'
-import { errorHandler, notFound } from './middleware/errorMiddleware.js';
-import connectDb from './config/db.js';
+import { errorHandler, notFound } from './middleware/errorMiddleware.js'
+import connectDb from './config/db.js'
 
 connectDb();
 
@@ -19,9 +20,18 @@ app.use(cookieParser())
 
 app.use('/api/v1/users', userRoutes)
 
-app.get("/", (req, res) => {
-  res.send('Server is ready')
-})  
+if (process.env.NODE_ENV === 'production') {
+  const __dirname = path.resolve();
+  app.use(express.static(path.join(__dirname, 'frontend/dist')))
+
+  app.get('*', (req, res) => res.sendFile(path.resolve(__dirname, 'frontend', 'dist', 'index.html')))
+} else {
+  app.get("/", (req, res) => {
+    res.send('Server is ready')
+  })  
+}
+
+
 
 app.use(notFound);
 app.use(errorHandler)
